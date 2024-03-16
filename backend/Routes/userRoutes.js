64248240d1622fbaa3558a25a1d19685136386f5
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-03-16 16:47:10 +0000

    UPDATED: user login test passed

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,15 +1,46 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import User from "../Models/User.js";
+
+// Create a new Router
 const router = express.Router();
+
+// POST Route for user registration
 
 router.post("/", async (req, res) => {
   try {
-    const user = new User(req.body);
+    // hash password set up
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user and hash password
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    // Save user to DB
     await user.save();
+
+    // send response status 201 and user id
     res.status(201).json({ user: user._id });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
+});
+
+router.post("/login", async (req, res) => {
+  // Find user
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) return res.status(400).send("Invalid username or password");
+
+  // Check Password
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).send("Invalid username or password");
+
+  // Send Response
+  res.send({ user: user._id });
 });
 
 export default router;
--- backend/test/userTests.spec.js
+++ backend/test/userTests.spec.js
@@ -1,10 +1,37 @@
 import * as chai from "chai";
 import { should } from "chai";
 import chaiHttp from "chai-http";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+import User from "../Models/User.js";
 import app from "../server.js";
 
 const { request } = chai.use(chaiHttp);
 should();
+
+// Set up for having existing user in DB before each test
+let userCredentials = {
+  username: "ronweasley",
+  password: "hogwarts2002",
+};
+
+before(async () => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(userCredentials.password, salt);
+
+    const user = new User({
+      username: userCredentials.username,
+      password: hashedPassword,
+    });
+
+    const savedUser = await user.save();
+    console.log(savedUser);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 describe("User Registration", () => {
   it("should create a new user", async function () {
@@ -29,13 +56,13 @@ describe("User Login", () => {
   it("Should Log in a user", async function () {
     // Arrange
     const userCredentials = {
-      username: "harrypotter",
-      password: "hogwarts2001",
+      username: "ronweasley",
+      password: "hogwarts2002",
     };
 
     // Act
 
-    const res = await response(app)
+    const res = await request(app)
       .post("/api/User/login")
       .send(userCredentials);
 
@@ -44,4 +71,8 @@ describe("User Login", () => {
     res.body.should.be.a("object");
     res.body.should.have.property("user");
   });
+});
+
+after(() => {
+  mongoose.connection.close();
 });
