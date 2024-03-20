ca5426382976023c71553331b8a1df38748076d2
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-03-20 19:16:28 +0000

    UPDATED: searchEngine sets location state correctly

--- frontend/vite-project/src/Components/searchEngine.jsx
+++ frontend/vite-project/src/Components/searchEngine.jsx
@@ -1,46 +1,40 @@
 import { useState } from "react";
-import FetchApiData from "../UtilityFunctions/FetchApiData.jsx";
 import "./searchEngine.css";
 
-const SearchEngine = ({ inNavbar }) => {
+const SearchEngine = ({ inNavbar, setLocation }) => {
   const [search, setSearch] = useState("");
-  const [submit, setSubmit] = useState(false);
 
   const className = inNavbar ? "navbar-search" : "search-container";
 
   const handleSearchChange = (event) => {
-    setSubmit(false);
     setSearch(event.target.value);
   };
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     console.log(search);
-    setSubmit(true);
+    setLocation(search);
   };
 
   return (
-    <>
-      <div className={className}>
-        <form onSubmit={handleSearchSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="locationInput"
-              placeholder="Enter Location"
-              value={search}
-              onChange={handleSearchChange}
-            />
-          </div>
+    <div className={className}>
+      <form onSubmit={handleSearchSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="locationInput"
+            placeholder="Enter Location"
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </div>
 
-          <button className="search-button" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-      {submit && <FetchApiData location={search} />}
-    </>
+        <button className="search-button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
--- frontend/vite-project/src/Pages/HomePage.jsx
+++ frontend/vite-project/src/Pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import SearchEngine from "../Components/searchEngine";
 import "./HomePage.css";
 
-const Home = () => {
+const Home = ({ location, setLocation }) => {
   return (
     <div className="home-container homepage">
       <h1>Tell Me About...</h1>
-      {<SearchEngine />}
+      {<SearchEngine setLocation={setLocation} />}
     </div>
   );
 };
