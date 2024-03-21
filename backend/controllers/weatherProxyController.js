d4224b4f3ef200a44cd1be457b3e0e653581fdd2
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-03-21 14:56:44 +0000

    UPDATED modified server with weather proxy route

--- backend/controllers/weatherProxyController.js
+++ backend/controllers/weatherProxyController.js
@@ -19,24 +19,3 @@ const getWeatherData = async (location) => {
 };
 
 export default getWeatherData;
-
-/*import axios from "axios";
-
-const FetchApiData = async (location) => {
-  if (!location) {
-    console.error("No location provided");
-    return;
-  }
-  try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=82eead94890f3e9d0203a62caa188ff4`
-    );
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(`There was a problem with the fetch operation`, error);
-    return {};
-  }
-};
-
-export default FetchApiData;*/
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import getWeatherData from "./controllers/weatherProxyController.js";
 import userRoutes from "./Routes/userRoutes.js";
 
 dotenv.config();
@@ -12,12 +13,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Login and Registration
 mongoose
   .connect(process.env.DB_CONNECTION)
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
 
 app.use("/api/User", userRoutes);
+
+// Weather Proxy Route
+app.get("/api/weather/:location", async (req, res) => {
+  const data = await getWeatherData(req.params.location);
+  res.json(data);
+});
 
 const server = app.listen(process.env.PORT, () =>
   console.log(
