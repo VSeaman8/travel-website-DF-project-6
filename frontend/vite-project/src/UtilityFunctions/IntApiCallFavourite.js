c90724d3618343a47239a49aa640288aa24797f2
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-04-08 08:53:37 +0100

    UPDATED: changed url to refelct backend, corrected getFavouriteLocations so that it has header

--- frontend/vite-project/src/UtilityFunctions/IntApiCallFavourite.js
+++ frontend/vite-project/src/UtilityFunctions/IntApiCallFavourite.js
@@ -1,9 +1,16 @@
+// Front to Backend Connection for all locations
+
 // connect to back end for all locations
 export const getFavouriteLocations = async (userId) => {
   console.log("getFavouriteLocations userId:", userId);
-  const url = `http://localhost:4003/api/user/${userId}/favourites`;
+  const url = `http://localhost:4003/api/${userId}/locations`;
   console.log("url:", url);
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      userid: userId,
+    },
+  });
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
@@ -12,11 +19,12 @@ export const getFavouriteLocations = async (userId) => {
 // connect to backend for adding to locations
 export const addFavouriteLocation = async (userId, location) => {
   const response = await fetch(
-    `http://localhost:4003/api/user/${userId}/favourites`,
+    `http://localhost:4003/api/${userId}/locations`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        userid: userId,
       },
       body: JSON.stringify({ location }),
     }
@@ -30,11 +38,12 @@ export const addFavouriteLocation = async (userId, location) => {
 // connect to backend for removing a location
 export const removeFavouriteLocation = async (userId, location) => {
   const response = await fetch(
-    `http://localhost:4003/api/user/${userId}/favourites`,
+    `http://localhost:4003/api/${userId}/locations`,
     {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
+        userid: userId,
       },
       body: JSON.stringify({ location }),
     }
